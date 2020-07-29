d3fbb56df9f1a11df8896c9f3d8668276b920e06
jchen <janetchen906@gmail.com>
2020-07-28 22:04:14 -0700

    landing page works now

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,6 +11,9 @@ import romaniaImg from '../img/romania.png';
 import nzImg from '../img/newzealand.png';
 import Santorini from '../img/greece-santorini.png';
 import Zakynthos from '../img/greece-zakynthos.png';
+import { fetchMedia } from "../redux/media/mediaActions";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const images = [
   {
@@ -64,8 +67,16 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+function handleClick(image) {
+  console.log(image);
+}
+
 export default function LandingPage() {
   const classes = useStyles();
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   return (
     <div>
       <h1 className={classes.title}>Where will your next journey take you?</h1>
@@ -79,7 +90,10 @@ export default function LandingPage() {
       >
         {images.map((image) => {
           return <Grid item xs={4} key={image.key}>
-            <Card className={classes.root}>
+            <Card className={classes.root} onClick={() => {
+                  dispatch(fetchMedia(image.location));
+                  history.push('/results');
+                }}>
               <CardMedia
                 component="img"
                 height="240"
@@ -87,12 +101,11 @@ export default function LandingPage() {
                 title={image.location}
               />
               <CardActions>
-                <Button size="small">{image.location}</Button>
+                <Button size="small" >{image.location}</Button>
               </CardActions>
             </Card>
           </Grid>
-        }
-        )}
+        })}
       </Grid>
     </div >
   );
