4886659fe2cdef8b28090e76fdfc6001f93c5228
stevens <stevensqiu@gmail.com>
2020-07-29 23:24:31 -0700

    withcredentials for backend API requests

--- src/components/Login.js
+++ src/components/Login.js
@@ -50,22 +50,6 @@ function Login() {
 
   return (
     <div>
-      {/* <div>
-        <h1>Register</h1>
-        <input placeholder='username' onChange={e => setRegisterUsername(e.target.value)} />
-        <input placeholder='password' onChange={e => setRegisterPassword(e.target.value)} />
-        <button onClick={register}>Submit</button>
-      </div>
-      <div>
-        <h1>Login</h1>
-        <input placeholder='username' onChange={e => setLoginUsername(e.target.value)} />
-        <input placeholder='password' onChange={e => setLoginPassword(e.target.value)} />
-        <button onClick={login}>Submit</button>
-      </div>
-      <div>
-        <h1>Get User</h1>
-        <button onClick={getUser}>Submit</button>
-      </div> */}
       <ButtonList />
     </div>
   );
