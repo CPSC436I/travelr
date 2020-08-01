cc00d26fe12e8658ce9f53c2df2663b9559bff61
Christine Vu <christine.vu@paihealth.com>
2020-07-31 21:01:24 -0700

    Incorporate GooglePlaces API + UI

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux';
-import React, { useEffect, } from 'react';
+import React, { useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Media from './Media'
+import Media from './Media';
 import { fetchFavourites } from '../redux';
+// import getPlaceDetails from '../../api/routes/places';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 
 const styles = makeStyles => ({
   root: {
@@ -11,22 +15,22 @@ const styles = makeStyles => ({
     margin: 'auto'
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   grid: {
     display: 'flex',
     alignItems: 'center',
     alignContent: 'center',
-    direction: 'row',
+    direction: 'row'
   }
 });
 
-function mergeMediaAndVideos (query, media, folders, videos) {
+function mergeMediaAndVideos (query, media, folders, videos, places) {
   let mediaContent = media.map((imgInState) => {
     return <Grid item xs={4} key={imgInState.id}>
-      <Media 
-        media={imgInState} 
-        query={query} 
+      <Media
+        media={imgInState}
+        query={query}
         saved={folders
           .find(folder => folder.images.find(img => img.id === imgInState.id) !== undefined) !== undefined}
       />
@@ -34,17 +38,31 @@ function mergeMediaAndVideos (query, media, folders, videos) {
   });
   let videoContent = videos.map((videoId) => {
     return <Grid item xs={6} key={videoId}>
-    <iframe id="ytplayer" type="text/html" width="540" height="360"
-         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
-         frameBorder="0"></iframe>
-    </Grid>
+      <Media
+        video={videoId}
+        query={query}
+        saved={folders
+          .find(folder => folder.images.find(img => img.id === videoId) !== undefined) !== undefined} />
+    </Grid>;
   });
   let allContent = mediaContent.concat(videoContent);
+  let placesContent = places.map((place) => {
+    if (place !== null) {
+      return <Grid item xs={4} key={place.photoUrl}>
+        <Media
+          place={place}
+          query={query}
+          saved={folders
+            .find(folder => folder.images.find(img => img.id === place.id) !== undefined) !== undefined}
+        />
+      </Grid>;
+    }
+  });
+  allContent = allContent.concat(placesContent);
   return allContent;
 }
 
-function Display({ query, media, folders, fetchFavourites, videos }) {
-
+function Display ({ query, media, folders, fetchFavourites, videos, places }) {
   useEffect(() => {
     fetchFavourites();
   }, []);
@@ -54,13 +72,13 @@ function Display({ query, media, folders, fetchFavourites, videos }) {
       container
       flexgrow={1}
       spacing={3}
-      direction="row"
-      justify="center"
-      alignContent="center"
+      direction='row'
+      justify='center'
+      alignContent='center'
     >
       {media.length === 0 ? (
         null
-      ) : mergeMediaAndVideos(query, media, folders, videos)
+      ) : mergeMediaAndVideos(query, media, folders, videos, places)
       }
     </Grid>
   );
@@ -71,7 +89,8 @@ const mapStateToProps = (state) => {
     query: state.media.query,
     media: state.media.results,
     folders: state.folders.folders,
-    videos: state.videos.ids
+    videos: state.videos.ids,
+    places: state.places.places
   };
 };
 
@@ -80,8 +99,8 @@ const mapDispatchToProps = (dispatch) => {
     fetchFavourites: () => {
       dispatch(fetchFavourites());
     }
-  }
-}
+  };
+};
 
 const DisplayContainer = connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Display));
 
--- src/components/Media.js
+++ src/components/Media.js
@@ -8,13 +8,12 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 
 import { connect } from 'react-redux';
-import { toggleSaveMedia } from '../redux/'
+import { toggleSaveMedia } from '../redux/';
 
 const findTitle = (tags) => {
-//   console.log('TAGS: ' + JSON.stringify(tags));
-  let tag = tags.find(tag => tag.type === "search");
+  let tag = tags.find(tag => tag.type === 'search');
   if (tag) {
-    return tags.find(tag => tag.type === "search");
+    return tags.find(tag => tag.type === 'search');
   } else {
     return {
       title: 'NO TAGS'
@@ -22,38 +21,84 @@ const findTitle = (tags) => {
   }
 };
 
-function Media({media, saved, toggleSaveMedia}) {
-    const toggleSave = () => {
-        toggleSaveMedia("default", media, !saved);
-    }
+function Media ({ media, saved, video, place, query, toggleSaveMedia }) {
+  const toggleMediaSave = () => {
+    media.mediaType = 'media';
+    toggleSaveMedia('default', media, !saved);
+  };
 
+  const togglePlaceSave = () => {
+    toggleSaveMedia('default', place, !saved);
+  };
+
+  const toggleVideoSave = () => {
+    let videoObj = {
+      id: video,
+      tags: [query],
+      mediaType: 'video'
+    };
+    toggleSaveMedia('default', videoObj, !saved);
+  };
+
+  if (media) {
     return (
-        <Card key={media.id}>
-            <CardMedia
-                component="img"
-                height="240"
-                image={media.urls.small}
-                title={media.description}
-            />
-            <CardContent>
-            </CardContent>
-            <CardActions>
-                <Button size="small" >{findTitle(media.tags).title}</Button>
-                <IconButton aria-label="add to favorites" onClick={toggleSave}>
-                    <FavoriteIcon color={saved ? "secondary": "disabled"} />
-                    
-                </IconButton>
-            </CardActions>
-        </Card>
-    )
+      <Card key={media.id}>
+        <CardMedia
+          component='img'
+          height='240'
+          image={media.urls.small}
+          title={media.description}
+        />
+        <CardContent />
+        <CardActions>
+          <Button size='small' >{findTitle(media.tags).title}</Button>
+          <IconButton aria-label='add to favorites' onClick={toggleMediaSave}>
+            <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
 
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  } else if (video) {
+    return (<Card>
+      <CardMedia
+        component='iframe'
+        height='237'
+        src={`https://www.youtube.com/embed/${video}`}
+        title={'video'}
+      />
+      <CardActions>
+        <Button size='small'>{query} Travel Vlog</Button>
+        <IconButton aria-label='add to favorites' onClick={toggleVideoSave}>
+          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+
+        </IconButton>
+      </CardActions>
+    </Card>);
+  } else if (place) {
+    return <Card key={place.photoUrl}>
+      <CardMedia
+        component='img'
+        height='240'
+        image={place.photoUrl}
+        title={place.name}
+      />
+      <CardContent />
+      <CardActions>
+        <Button size='small' >{place.name}</Button>
+        <IconButton aria-label='add to favorites' onClick={togglePlaceSave}>
+          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+        </IconButton>
+      </CardActions>
+    </Card>;
+  }
 }
 const mapDispatchToProps = (dispatch) => {
-    return {
-        toggleSaveMedia: (folder, destination, shouldSave) => {
-            dispatch(toggleSaveMedia(folder, destination, shouldSave));
-        },
-    };
+  return {
+    toggleSaveMedia: (folder, destination, shouldSave) => {
+      dispatch(toggleSaveMedia(folder, destination, shouldSave));
+    }
+  };
 };
 
 export default connect(null, mapDispatchToProps)(Media);
