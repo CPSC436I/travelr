3c42d6c414993bb7019e52c067158323ec9b5f8f
jchen <janetchen906@gmail.com>
2020-08-04 22:56:25 -0700

    card modal

--- src/components/Media.js
+++ src/components/Media.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
-import {Button, Modal, makeStyles} from '@material-ui/core/';
+import { Button, Modal, makeStyles } from '@material-ui/core/';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -15,6 +15,11 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  images: {
+    borderRadius: 5,
+    boxShadow: theme.shadows[2],
+    margin: theme.spacing(2)
+  },
   paper: {
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
@@ -22,9 +27,17 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 5,
     outline: 'none'
   },
+  both: {
+    alignItems: 'center',
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    borderRadius: 5,
+    outline: 'none'
+  }
 }));
 
-function Media ({ media, saved, video, place, restaurant, event, query, toggleSaveMedia }) {
+function Media({ media, saved, video, place, restaurant, event, query, toggleSaveMedia }) {
   const classes = useStyles();
   const toggleMediaSave = () => {
     query = sessionStorage.getItem('query');
@@ -58,7 +71,6 @@ function Media ({ media, saved, video, place, restaurant, event, query, toggleSa
   }
 
   const [open, setOpen] = React.useState(false);
-  const [title] = React.useState('');
 
   const handleOpen = () => {
     setOpen(true);
@@ -69,24 +81,31 @@ function Media ({ media, saved, video, place, restaurant, event, query, toggleSa
   };
 
   const body = (
-    <div className={classes.paper}>
-      <h2 id="simple-modal-title">More information</h2>
-      <p id="simple-modal-description">
-        Coming soon {title}
-      </p>
+    <div className={classes.both}>
+      <h2 id="simple-modal-title" align-text="center">Information</h2>
+      <div>
+        <img className={classes.images} src="https://images.unsplash.com/photo-1549643276-fdf2fab574f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80" alt="vacation1" height="300" />
+        <img className={classes.images} src="https://images.unsplash.com/photo-1488462237308-ecaa28b729d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=608&q=80" alt="vacation2" height="300" />
+        <img className={classes.images} src="https://images.unsplash.com/photo-1503174768371-25049f8bf3f7?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="vacation3" height="300" />
+      </div>
+        <ul id="simple-modal-description">
+        <li>Location: <a href="https://bitly.com/98K8eH">link to Google Maps</a></li>
+        <li> Rating: 4.2/5 (based on 128 reviews)</li>
+        <li>Description: to be honest, this part isn't completed yet, so I'm just writing this as a filler for future content.<br></br>If you're curious about the next goals of the project, you can click <a href="https://github.com/CPSC436I/travelr">here</a></li>
+        </ul>
     </div>
   );
 
   const infoModal = (
     <Modal
-        open={open}
-        onClose={handleClose}
-        className={classes.modal}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
+      open={open}
+      onClose={handleClose}
+      className={classes.modal}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      {body}
+    </Modal>
   );
 
   if (media) {
@@ -111,77 +130,77 @@ function Media ({ media, saved, video, place, restaurant, event, query, toggleSa
     </div>
   } else if (video) {
     return <div>
-    <Card>
-      <CardMedia
-        id='iframeM'
-        component='iframe'
-        height='400'
-        src={`https://www.youtube.com/embed/${video}`}
-        title={'video'}
-      />
-      <CardActions>
-        <Button size='small' onClick={handleOpen}> Travel Vlog</Button>
-        <IconButton aria-label='add to favorites' onClick={toggleVideoSave}>
-          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
-        </IconButton>
-      </CardActions>
-    </Card>
-    {infoModal}
+      <Card>
+        <CardMedia
+          id='iframeM'
+          component='iframe'
+          height='400'
+          src={`https://www.youtube.com/embed/${video}`}
+          title={'video'}
+        />
+        <CardActions>
+          <Button size='small' onClick={handleOpen}> Travel Vlog</Button>
+          <IconButton aria-label='add to favorites' onClick={toggleVideoSave}>
+            <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+          </IconButton>
+        </CardActions>
+      </Card>
+      {infoModal}
     </div>
-    ;
+      ;
   } else if (place) {
     return <div>
-    <Card key={place.photoUrl}>
-      <CardMedia
-        component='img'
-        height='240'
-        image={place.photoUrl}
-        title={place.name}
-      />
-      <CardActions>
-        <Button size='small' onClick={handleOpen}>{place.name}</Button>
-        <IconButton aria-label='add to favorites' onClick={togglePlaceSave}>
-          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
-        </IconButton>
-      </CardActions>
-    </Card>
-    {infoModal}
+      <Card key={place.photoUrl}>
+        <CardMedia
+          component='img'
+          height='240'
+          image={place.photoUrl}
+          title={place.name}
+        />
+        <CardActions>
+          <Button size='small' onClick={handleOpen}>{place.name}</Button>
+          <IconButton aria-label='add to favorites' onClick={togglePlaceSave}>
+            <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+          </IconButton>
+        </CardActions>
+      </Card>
+      {infoModal}
     </div>;
   } else if (restaurant) {
     return <div>
-    <Card key={restaurant.id}>
-      <CardMedia
-        component='img'
-        height='240'
-        image={restaurant.photoUrl}
-        title={restaurant.name}
-      />
-      <CardActions>
-        <Button size='small' onClick={handleOpen}>{restaurant.name}</Button>
-        <IconButton aria-label='add to favorites' onClick={toggleRestaurantSave}>
-          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
-        </IconButton>
-      </CardActions>
-    </Card>
-    {infoModal}
+      <Card key={restaurant.id}>
+        <CardMedia
+          component='img'
+          height='240'
+          image={restaurant.photoUrl}
+          title={restaurant.name}
+        />
+        <CardActions>
+          <Button size='small' onClick={handleOpen}>{restaurant.name}</Button>
+          <IconButton aria-label='add to favorites' onClick={toggleRestaurantSave}>
+            <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+          </IconButton>
+        </CardActions>
+      </Card>
+      {infoModal}
     </div>;
   } else if (event) {
     return <div>
-    <Card key={event.id}>
-      <CardMedia
-        component='img'
-        height='240'
-        image={event.photoUrl}
-        title={event.name}
-      />
-      <CardActions>
-        <Button size='small' onClick={handleOpen}>{event.name}</Button>
-        <IconButton aria-label='add to favorites' onClick={toggleEventSave}>
-          <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
-        </IconButton>
-      </CardActions>
-    </Card>
-    {infoModal}
+      <Card key={event.id}>
+        <CardMedia
+          component='img'
+          height='240'
+          image={event.photoUrl}
+          title={event.name}
+        />
+        <CardActions>
+          <Button size='small' onClick={handleOpen}>{event.name}</Button>
+          <IconButton aria-label='add to favorites' onClick={toggleEventSave}>
+            <FavoriteIcon color={saved ? 'secondary' : 'disabled'} />
+          </IconButton>
+        </CardActions>
+      </Card>
+      {infoModal}
     </div>
   } else {
     return null;
